79c4e32acc8f0cdf5f51bbff8a6b10b528376050
Amin <safavi.m.amin@gmail.com>
2025-09-14 16:52:38 +0900

    Refactor build_and_test.sh to use project root directory for build path

--- tools/build_and_test.sh
+++ tools/build_and_test.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-cd eigen_tidy_plugin
+# Get the root directory of the project
+ROOT_DIR=$(git rev-parse --show-toplevel)
 
-# Create build directory if it doesn't exist
-mkdir -p build
-
-# Navigate to the build directory
-cd build
+# Create and navigate to the build directory
+BUILD_DIR="$ROOT_DIR/build"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 
 # Run CMake to configure the project
-cmake .. -DBUILD_TESTING=ON
+cmake "$ROOT_DIR/eigen_tidy_plugin" -DBUILD_TESTING=ON
 
 # Build the project
 make -j$(nproc)
